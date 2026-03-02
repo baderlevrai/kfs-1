ea9b3616d7dfb0d7c77cdd5f11fc8cbda72deaf6
Bader <142030077+baderlevrai@users.noreply.github.com>
2026-03-02 14:50:10 +0100

    feat: printk
    * feat: added printk and basics format
    
    * feat: added pointer print
    
    * fix: fixed compilation errors
    
    ---------
    
    Co-authored-by: bader <bzaidi@f6r7s14.clusters.42paris.fr>
    Co-authored-by: bader <bzaidi@f1br1s8.paris.42.school>

--- src/kernel.h
+++ src/kernel.h
@@ -50,6 +50,8 @@ size_t	strlen(const char* str);
 void	kbd_handler(void);
 void	kbd_init(void);
 
+int printk(const char *format, ...);
+
 inline void outb(uint16_t port, uint8_t val)
 {
 	asm volatile ("outb %b0, %w1" : : "a"(val), "Nd"(port) : "memory");
